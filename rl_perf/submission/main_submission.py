7e023d6e3732d5c67dc35022a39295c303a4b610
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-02-19 11:42:40 -0500

    can run webnav through submission process

--- rl_perf/submission/main_submission.py
+++ rl_perf/submission/main_submission.py
@@ -1,18 +1,24 @@
-import os
-from absl import app
 import importlib.util
+import os
 
-
-def run_benchmark(participant_module):
-    pass
+from absl import app
+import logging
+from submission import Submission
 
 
 def main(_):
-    location = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'rlperf_benchmark_submission'))
-    spec = importlib.util.spec_from_file_location("train", location)
-    train = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(train)
-    train.main()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    participant_module_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..', 'rlperf_benchmark_submission', 'train.py'))
+    participant_module_spec = importlib.util.spec_from_file_location("train", participant_module_path)
+    participant_module = importlib.util.module_from_spec(participant_module_spec)
+
+    submission = Submission(participant_module=participant_module, participant_module_path=participant_module_path,
+                            participant_module_spec=participant_module_spec,
+                            benchmark='web_navigation', mode='train')
+    submission.run_benchmark()
 
 
 if __name__ == '__main__':
