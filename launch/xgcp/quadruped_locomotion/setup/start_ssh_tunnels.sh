67ee1c4b1e2b2c87647f81813263f445a446f4ee
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-09-14 00:42:44 +0000

    adjusting xgcp launch files for xgcp

--- launch/xgcp/quadruped_locomotion/setup/start_ssh_tunnels.sh
+++ launch/xgcp/quadruped_locomotion/setup/start_ssh_tunnels.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-declare -a servers=("locomotion-0" "locomotion-1" "locomotion-2" "locomotion-3" "locomotion-4" "locomotion-5")
-declare -a zones=("us-central1-a" "us-central1-a" "us-east4-a" "us-east4-a" "us-west1-a" "us-west1-a")
+#declare -a servers=("locomotion-0" "locomotion-1" "locomotion-2" "locomotion-3" "locomotion-4" "locomotion-5")
+#declare -a zones=("us-central1-a" "us-central1-a" "us-east4-a" "us-east4-a" "us-west1-a" "us-west1-a")
 
-local_port=10999
+declare -a servers=("locomotion-3" "locomotion-4" "locomotion-5")
+declare -a zones=("us-east4-a" "us-west1-a" "us-west1-a")
+#local_port=10999
+local_port=11002
 
 # Create a new detached tmux session with a dummy command
-tmux new-session -d -s tunnels_locomotion "echo 'Tunnels'; zsh"
+tmux new-session -d -s tunnels_locomotion_sac "echo 'Tunnels'; zsh"
 
 # Loop through each server using its index and establish the SSH tunnel
 for index in "${!servers[@]}"; do
@@ -17,7 +20,7 @@ for index in "${!servers[@]}"; do
   echo "Establishing tunnel for $server in zone $zone on local port $local_port..."
 
   # Run expect within tmux, also adding the zone parameter
-  tmux new-window -t tunnels_locomotion -n "$server" "/usr/bin/expect -c \"
+  tmux new-window -t tunnels_locomotion_sac -n "$server" "/usr/bin/expect -c \"
         set timeout -1
         spawn gcloud compute ssh $server --zone $zone --tunnel-through-iap -- -L $local_port:localhost:22
         expect \\\"Enter passphrase for key '/usr/local/google/home/ikechukwuu/.ssh/google_compute_engine':\\\"
