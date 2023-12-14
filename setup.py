33b2df313c3a3b25cbb10ebb8358a37f9bd57ee2
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-12-14 17:16:24 -0500

    Did some more renaming

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='rl_perf',
-        version='0.1',
-        packages=find_packages(),
-        install_requires=[
-                'numpy',
-                'psutil',
-                'gin-config',
-                ],
-        )
+    name='a2perf',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'psutil',
+        'gin-config',
+        'gymnasium',
+        'minari',
+        'absl-py'
+    ],
+)
