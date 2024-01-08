71a6601fa063362eacafef435ec9d44f3c4c19a6
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-01-08 00:19:33 +0000

    Added cuda malloc async for cluster

--- launch/entrypoints/quadruped_locomotion.py
+++ launch/entrypoints/quadruped_locomotion.py
@@ -6,44 +6,54 @@ from absl import flags
 
 _DEBUG = flags.DEFINE_bool('debug', False, 'Debugging mode')
 _SEED = flags.DEFINE_integer('seed', None, 'Global seed.')
-_ENV_BATCH_SIZE = flags.DEFINE_integer('env_batch_size', None,
-                                       'Number of environments to run in parallel.')
+_ENV_BATCH_SIZE = flags.DEFINE_integer(
+    'env_batch_size', None, 'Number of environments to run in parallel.'
+)
 _NUM_EPOCHS = flags.DEFINE_integer('num_epochs', None, 'Number of epochs.')
-_TOTAL_ENV_STEPS = flags.DEFINE_integer('total_env_steps', None,
-                                        'Total steps in the environment.')
-_MOTION_FILE_PATH = flags.DEFINE_string('motion_file_path', None,
-                                        'Motion file path.')
+_TOTAL_ENV_STEPS = flags.DEFINE_integer(
+    'total_env_steps', None, 'Total steps in the environment.'
+)
+_MOTION_FILE_PATH = flags.DEFINE_string(
+    'motion_file_path', None, 'Motion file path.'
+)
 _ALGO = flags.DEFINE_string('algo', None, 'Algorithm used.')
-_SKILL_LEVEL = flags.DEFINE_enum('skill_level', None,
-                                 ['novice', 'intermediate', 'expert'],
-                                 'Skill level of the expert.')
-_TASK = flags.DEFINE_enum('task', None, ['dog_pace', 'dog_trot', 'dog_spin'],
-                          'Task to run.')
+_SKILL_LEVEL = flags.DEFINE_enum(
+    'skill_level',
+    None,
+    ['novice', 'intermediate', 'expert'],
+    'Skill level of the expert.',
+)
+_TASK = flags.DEFINE_enum(
+    'task', None, ['dog_pace', 'dog_trot', 'dog_spin'], 'Task to run.'
+)
 _GIN_CONFIG = flags.DEFINE_string('gin_config', None, 'Gin config file.')
 _MODE = flags.DEFINE_string('mode', None, 'Mode of execution.')
 _LEARNING_RATE = flags.DEFINE_float('learning_rate', None, 'Learning rate.')
-_BATCH_SIZE = flags.DEFINE_integer('batch_size', None,
-                                   'Batch size for training.')
+_BATCH_SIZE = flags.DEFINE_integer(
+    'batch_size', None, 'Batch size for training.'
+)
 _ROOT_DIR = flags.DEFINE_string('root_dir', None, 'Root directory.')
-_PARTICIPANT_MODULE_PATH = flags.DEFINE_string('participant_module_path', None,
-                                               'Participant module path.')
-_RUN_OFFLINE_METRICS_ONLY = flags.DEFINE_boolean('run_offline_metrics_only',
-                                                 None,
-                                                 'Run offline metrics only.')
-_TIMESTEPS_PER_ACTORBATCH = flags.DEFINE_integer('timesteps_per_actorbatch',
-                                                 None,
-                                                 'Timesteps per actor batch.')
-_TRAIN_CHECKPOINT_INTERVAL = flags.DEFINE_integer('train_checkpoint_interval',
-                                                  None,
-                                                  'Train checkpoint interval.')
-_POLICY_CHECKPOINT_INTERVAL = flags.DEFINE_integer('policy_checkpoint_interval',
-                                                   None,
-                                                   'Policy checkpoint interval.')
+_PARTICIPANT_MODULE_PATH = flags.DEFINE_string(
+    'participant_module_path', None, 'Participant module path.'
+)
+_RUN_OFFLINE_METRICS_ONLY = flags.DEFINE_boolean(
+    'run_offline_metrics_only', None, 'Run offline metrics only.'
+)
+_TIMESTEPS_PER_ACTORBATCH = flags.DEFINE_integer(
+    'timesteps_per_actorbatch', None, 'Timesteps per actor batch.'
+)
+_TRAIN_CHECKPOINT_INTERVAL = flags.DEFINE_integer(
+    'train_checkpoint_interval', None, 'Train checkpoint interval.'
+)
+_POLICY_CHECKPOINT_INTERVAL = flags.DEFINE_integer(
+    'policy_checkpoint_interval', None, 'Policy checkpoint interval.'
+)
 _EVAL_INTERVAL = flags.DEFINE_integer('eval_interval', None, 'Eval interval.')
 _LOG_INTERVAL = flags.DEFINE_integer('log_interval', None, 'Log interval.')
 
-_ENTROPY_REGULARIZATION = flags.DEFINE_float('entropy_regularization', None,
-                                             'Entropy regularization.')
+_ENTROPY_REGULARIZATION = flags.DEFINE_float(
+    'entropy_regularization', None, 'Entropy regularization.'
+)
 _DATASET_ID = flags.DEFINE_string('dataset_id', None, 'Dataset ID.')
 
 
@@ -63,21 +73,22 @@ def main(_):
   os.environ['ROOT_DIR'] = _ROOT_DIR.value
   os.environ['PARTICIPANT_MODULE_PATH'] = _PARTICIPANT_MODULE_PATH.value
   os.environ['RUN_OFFLINE_METRICS_ONLY'] = str(_RUN_OFFLINE_METRICS_ONLY.value)
-  os.environ['TIMESTEPS_PER_ACTOR_BATCH'] = str(
-      _TIMESTEPS_PER_ACTORBATCH.value)
+  os.environ['TIMESTEPS_PER_ACTOR_BATCH'] = str(_TIMESTEPS_PER_ACTORBATCH.value)
   os.environ['TRAIN_CHECKPOINT_INTERVAL'] = str(
-      _TRAIN_CHECKPOINT_INTERVAL.value)
+      _TRAIN_CHECKPOINT_INTERVAL.value
+  )
   os.environ['POLICY_CHECKPOINT_INTERVAL'] = str(
-      _POLICY_CHECKPOINT_INTERVAL.value)
+      _POLICY_CHECKPOINT_INTERVAL.value
+  )
   os.environ['EVAL_INTERVAL'] = str(_EVAL_INTERVAL.value)
   os.environ['LOG_INTERVAL'] = str(_LOG_INTERVAL.value)
   os.environ['WRAPT_DISABLE_EXTENSIONS'] = 'true'
   os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+  os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
   os.environ['ENTROPY_REGULARIZATION'] = str(_ENTROPY_REGULARIZATION.value)
   os.environ['DATASET_ID'] = _DATASET_ID.value
   os.environ['DEBUG'] = str(_DEBUG.value)
-  os.environ['TIMESTEPS_PER_ACTORBATCH'] = str(
-      _TIMESTEPS_PER_ACTORBATCH.value)
+  os.environ['TIMESTEPS_PER_ACTORBATCH'] = str(_TIMESTEPS_PER_ACTORBATCH.value)
   command = (
       'python3.9 a2perf/submission/main_submission.py '
       f'--gin_config={_GIN_CONFIG.value} '
@@ -86,8 +97,9 @@ def main(_):
       f'--run_offline_metrics_only={_RUN_OFFLINE_METRICS_ONLY.value}'
   )
 
-  process = subprocess.Popen(command, stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT, shell=True)
+  process = subprocess.Popen(
+      command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+  )
   while True:
     output = process.stdout.readline()
     if process.poll() is not None:
