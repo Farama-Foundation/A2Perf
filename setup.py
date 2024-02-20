4cc30d532790c7b1b90536f8b06d568b596fa626
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-02-20 15:16:58 +0000

    Automatically downloading dreamplace for user

--- setup.py
+++ setup.py
@@ -1,5 +1,59 @@
+import os
+import shutil
+import site
+import sys
+import urllib.request
+
+from absl import logging
+from setuptools import Command
 from setuptools import find_packages
 from setuptools import setup
+
+
+class DreamplaceInstall(Command):
+  """Custom command to download and extract Dreamplace."""
+  user_options = []
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    # Dynamically determine the dreamplace version based on the Python version
+    python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+    dreamplace_version = f"dreamplace_python{python_version}.tar.gz"
+    logging.info(f"Installing Dreamplace version {dreamplace_version}")
+
+    dreamplace_url = f"https://storage.googleapis.com/rl-infra-public/circuit-training/dreamplace/{dreamplace_version}"
+    logging.info(f"Downloading Dreamplace from {dreamplace_url}")
+
+    site_packages_path = site.getsitepackages()[0]
+    dreamplace_dir = os.path.join(site_packages_path, 'dreamplace')
+    logging.info(f"Installing Dreamplace to {dreamplace_dir}")
+
+    os.makedirs(dreamplace_dir, exist_ok=True)
+    tar_path = os.path.join(dreamplace_dir, "dreamplace.tar.gz")
+
+    logging.info(f"Downloading Dreamplace to {tar_path}")
+    urllib.request.urlretrieve(dreamplace_url, tar_path)
+
+    logging.info('Extracting Dreamplace')
+    shutil.unpack_archive(tar_path, dreamplace_dir)
+
+    logging.info('Dreamplace installed')
+
+    # Create an __init__.py file in the top-level Dreamplace directory
+    init_path = os.path.join(dreamplace_dir, '__init__.py')
+    with open(init_path, 'w') as init_file:
+      init_file.write(
+          "from .dreamplace import ("
+          "PlaceDB, Params, NonLinearPlace, Placer, "
+          "PlaceObj, EvalMetrics, BasicPlace, "
+          "NesterovAcceleratedGradientOptimizer)\n"
+      )
+
 
 setup(
     name='a2perf',
@@ -13,6 +67,9 @@ setup(
         'minari',
         'absl-py'
     ],
+    cmdclass={
+        'dreamplace_install': DreamplaceInstall,
+    },
     package_data={
         'a2perf': [
             # Include the default gin config files for running the becnhmark
