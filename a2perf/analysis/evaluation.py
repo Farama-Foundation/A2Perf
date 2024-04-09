93aeb11f9ff3efb5bb99e45603a90e6adcdd9bd8
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-04-09 19:13:59 +0000

    Updated eval script to track the training energy cost

--- a2perf/analysis/evaluation.py
+++ a2perf/analysis/evaluation.py
@@ -4,12 +4,14 @@ import multiprocessing
 import os
 from typing import Any
 from typing import Dict
+from typing import Tuple
 
 import numpy as np
 from absl import app
 from absl import flags
 from absl import logging
 
+from a2perf.analysis.metrics_lib import load_training_system_data
 # noinspection PyUnresolvedReferences
 from a2perf.domains import circuit_training
 # noinspection PyUnresolvedReferences
@@ -57,13 +59,28 @@ def load_policy_and_perform_rollouts(
           'max': np.max(episode_returns).astype(float),
           'median': np.median(episode_returns).astype(float),
           'count': int(episode_returns.size),
-          'values': [float(v) for v in episode_returns],
+          'rollout_returns': [float(v) for v in episode_returns],
       }
   }
 
   logging.info('Evaluation results for %s:', checkpoint_path)
   logging.info('\t%s', eval_dict[checkpoint_path])
   return eval_dict
+
+
+def add_training_energy_cost(checkpoint_item: Tuple[str, Dict[str, Any]],
+    total_energy_kwh) -> Tuple[str, Dict[str, Any]]:
+  checkpoint_path = checkpoint_item[0]
+  checkpoint_dict = checkpoint_item[1]
+
+  policy_checkpoint_name = os.path.basename(checkpoint_path)
+  policy_checkpoint_number = int(policy_checkpoint_name.split('_')[-1])
+
+  checkpoint_dict.update(
+      {'total_training_energy_kwh': total_energy_kwh,
+       'training_energy_kwh': total_energy_kwh * policy_checkpoint_number,
+       'checkpoint_number': policy_checkpoint_number})
+  return checkpoint_path, checkpoint_dict
 
 
 def main(_):
@@ -93,6 +110,48 @@ def main(_):
     pool.join()
 
   all_episode_returns = {k: v for d in episode_returns for k, v in d.items()}
+
+  # Use the experiment path to get the system metrics for this training run
+  experiment_path = os.path.abspath(
+      os.path.join(_ROOT_DIR.value, os.pardir, os.pardir, os.pardir, os.pardir))
+  logging.debug('Experiment path: %s', experiment_path)
+  training_system_df = load_training_system_data(
+      base_dir=os.path.abspath(os.path.join(experiment_path, os.pardir)),
+      experiment_ids=[os.path.basename(experiment_path)])
+
+  # For each run-id, take the last `energy_consumed` entry and sum them together
+  total_training_energy_kwh = training_system_df.groupby(
+      ['domain', 'algo', 'task', 'experiment', 'seed']
+  )['energy_consumed'].last().sum()
+
+  # Add the training sample cost to the evaluation results
+  with multiprocessing.Pool(_MAX_PARALLEL_ENVS.value) as pool:
+    all_episode_returns = pool.map(
+        functools.partial(add_training_energy_cost,
+                          total_energy_kwh=total_training_energy_kwh),
+        all_episode_returns.items()
+    )
+    pool.close()
+    pool.join()
+
+  # Turn all_episode_returns back into a dictionary
+  all_episode_returns = {k: v for (k, v) in all_episode_returns}
+
+  maximum_checkpoint_number = max(
+      [v['checkpoint_number'] for v in all_episode_returns.values()]
+  )
+  logging.info('Maximum checkpoint number: %d', maximum_checkpoint_number)
+
+  for checkpoint_path, checkpoint_dict in all_episode_returns.items():
+    # Adjusting the training energy cost such that earlier checkpoints are
+    # associated with less energy usage
+    checkpoint_dict['training_energy_kwh'] = checkpoint_dict[
+                                               'training_energy_kwh'] / maximum_checkpoint_number
+
+    # Make sure that the energy usage for the final checkpoint is the same as
+    # the total energy usage
+    if checkpoint_dict['checkpoint_number'] == maximum_checkpoint_number:
+      assert checkpoint_dict['training_energy_kwh'] == total_training_energy_kwh
 
   # Save as JSON
   evaluation_save_path = os.path.join(
