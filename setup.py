3b9e2c29efa27c6a8859a4288bd4824d3b8e443d
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-06-11 19:29:44 -0400

    Moved dependencies to pyproject.toml. Updated custom command for circuit training specifically

--- setup.py
+++ setup.py
@@ -1,176 +1,127 @@
+"""Sets up the project."""
+
+import importlib.util
 import logging
 import os
+import pathlib
 import shutil
 import site
 import sys
 import urllib.request
-from zipfile import ZipFile
+from distutils.cmd import Command
 
-import pkg_resources
-from setuptools import find_packages
 from setuptools import setup
-from setuptools.command.install import install
+
+logging.basicConfig(level=logging.INFO)
+
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the a2perf version."""
+    path = CWD / "a2perf" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
 
 
 def install_dreamplace():
-  # Dynamically determine the dreamplace version based on the Python version
-  python_version = f'{sys.version_info.major}.{sys.version_info.minor}'
-  dreamplace_version = f'dreamplace_python{python_version}.tar.gz'
-  logging.info(f'Installing Dreamplace version {dreamplace_version}')
+    # Dynamically determine the dreamplace version based on the Python version
+    python_version = "%d.%d" % (sys.version_info.major, sys.version_info.minor)
+    dreamplace_version = "dreamplace_python%s.tar.gz" % python_version
+    logging.info("Installing Dreamplace version %s", dreamplace_version)
 
-  dreamplace_url = f'https://storage.googleapis.com/rl-infra-public/circuit-training/dreamplace/{dreamplace_version}'
-  logging.info(f'Downloading Dreamplace from {dreamplace_url}')
+    dreamplace_url = (
+        "https://storage.googleapis.com/rl-infra-public/circuit-training/dreamplace/%s"
+        % dreamplace_version
+    )
+    logging.info("Downloading Dreamplace from %s", dreamplace_url)
 
-  site_packages_path = site.getsitepackages()[0]
-  dreamplace_dir = os.path.join(site_packages_path, 'dreamplace')
-  logging.info(f'Installing Dreamplace to {dreamplace_dir}')
+    site_packages_path = site.getsitepackages()[0]
+    dreamplace_dir = os.path.join(site_packages_path, "dreamplace")
+    logging.info("Installing Dreamplace to %s", dreamplace_dir)
 
-  os.makedirs(dreamplace_dir, exist_ok=True)
-  tar_path = os.path.join(dreamplace_dir, 'dreamplace.tar.gz')
+    os.makedirs(dreamplace_dir, exist_ok=True)
+    tar_path = os.path.join(dreamplace_dir, "dreamplace.tar.gz")
 
-  logging.info(f'Downloading Dreamplace to {tar_path}')
-  urllib.request.urlretrieve(dreamplace_url, tar_path)
+    logging.info("Downloading Dreamplace to %s", tar_path)
+    urllib.request.urlretrieve(dreamplace_url, tar_path)
 
-  logging.info('Extracting Dreamplace')
-  shutil.unpack_archive(tar_path, dreamplace_dir)
+    logging.info("Extracting Dreamplace")
+    shutil.unpack_archive(tar_path, dreamplace_dir)
 
-  logging.info('Dreamplace installed')
+    logging.info("Dreamplace installed")
 
-  # Create a .pth file in the site-packages directory
-  dreamplace_inner_dir = os.path.join(dreamplace_dir, 'dreamplace')
-  dreamplace_outer_pth = os.path.join(site_packages_path, 'dreamplace.pth')
-  with open(dreamplace_outer_pth, 'w') as file:
-    file.write(dreamplace_dir + '\n')
+    # Create a .pth file in the site-packages directory
+    dreamplace_inner_dir = os.path.join(dreamplace_dir, "dreamplace")
+    dreamplace_outer_pth = os.path.join(site_packages_path, "dreamplace.pth")
+    with open(dreamplace_outer_pth, "w") as file:
+        file.write(dreamplace_dir + "\n")
 
-  dreamplace_inner_pth = os.path.join(
-      site_packages_path, 'dreamplace_dreamplace.pth'
-  )
-  with open(dreamplace_inner_pth, 'w') as file:
-    file.write(dreamplace_inner_dir + '\n')
+    dreamplace_inner_pth = os.path.join(site_packages_path, "dreamplace_dreamplace.pth")
+    with open(dreamplace_inner_pth, "w") as file:
+        file.write(dreamplace_inner_dir + "\n")
 
 
 def set_executable_permissions():
-  """Set the executable permissions for the plc_wrapper_main binary and raise an error if it fails."""
-  binary_path = os.path.join(sys.prefix, 'bin', 'plc_wrapper_main')
+    """Set the executable permissions for the plc_wrapper_main binary and raise an error if it fails."""
+    package_dir = os.path.dirname(importlib.util.find_spec("a2perf").origin)
+    binary_path = os.path.join(
+        package_dir, "domains", "circuit_training", "bin", "plc_wrapper_main"
+    )
+    logging.info("Location of plc_wrapper_main: %s", binary_path)
+    if os.path.exists(binary_path):
+        try:
+            os.chmod(binary_path, 0o755)
+            logging.info("Executable permissions set for plc_wrapper_main.")
+        except Exception as e:
+            raise RuntimeError(
+                "Failed to set executable permissions for plc_wrapper_main: %s" % e
+            )
+    else:
+        # Raise an exception if the file is not found
+        raise FileNotFoundError("plc_wrapper_main not found at expected path.")
 
-  if os.path.exists(binary_path):
-    try:
-      os.chmod(binary_path, 0o755)
-      logging.info('Executable permissions set for plc_wrapper_main.')
-    except Exception as e:
-      # Raise an exception to halt the installation
-      raise RuntimeError(
-          f'Failed to set executable permissions for plc_wrapper_main: {e}'
-      )
-  else:
-    # Raise an exception if the file is not found
-    raise FileNotFoundError('plc_wrapper_main not found at expected path.')
 
+class InstallCircuitTraining(Command):
+    description = "Install circuit training dependencies and setup."
+    user_options = []
 
-class CustomInstall(install):
-  """Custom installation script to include Dreamplace and plc_wrapper_main installation."""
+    def initialize_options(self):
+        pass
 
-  def run(self):
-    install.run(self)
-    install_dreamplace()
-    set_executable_permissions()
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        install_dreamplace()
+        set_executable_permissions()
 
 
 setup(
-    name='a2perf',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[
-        'psutil',
-        'gin-config',
-        'gymnasium',
-        'gym',
-        'minari',
-        'absl-py',
-        # codecarbon
-        'arrow',
-        'dash',
-        'dash_bootstrap_components',
-        'dataclasses',
-        'fire',
-        'flake8',
-        'fuzzywuzzy',
-        'isort',
-        'mypy',
-        'pandas',
-        'plotly',
-        'psutil',
-        'py-cpuinfo',
-        'pynvml',
-        'requests',
-        'responses',
-        'sphinx',
-        'sphinx-rtd-theme',
-        'fuzzywuzzy',
-        'pandas',
-        'pyfiglet',
-        'scipy',
-    ],
-    extras_require={
-        'circuit-training': [
-            'torch==1.13.1',
-            'tensorflow',
-            'tf-agents',
-            'timeout-decorator',
-            'matplotlib',
-            'cairocffi',
-            'shapely',
-        ],
-        'web-navigation': ['selenium', 'regex', 'chromedriver-py'],
-        'quadruped-locomotion': ['pybullet'],
-        'all': [
-            'torch==1.13.1',
-            'selenium',
-            'chromedriver-py',
-            'pybullet',
-            'tensorflow',
-            'regex',
-            'tf-agents',
-            'timeout-decorator',
-            'matplotlib',
-            'cairocffi',
-            'shapely',
-        ],
-    },
+    name="a2perf",
+    version=get_version(),
+    long_description=get_description(),
     cmdclass={
-        'install': CustomInstall,
-    },
-    data_files=[
-        ('bin', ['bin/plc_wrapper_main']),
-    ],
-    package_data={
-        'a2perf': [
-            # Include the default gin config files for running the benchmark
-            'submission/**/*.gin',
-            # Include default gin config files for each domain
-            'domains/web_navigation/configs/web_navigation_env_config.gin',
-            'domains/quadruped_locomotion/motion_imitation/configs/envdesign.gin',
-            'domains/circuit_training/circuit_training/configs/envdesign.gin',
-            # Include designs for creating web navigation environments
-            'domains/web_navigation/environment_generation/data/*.json',
-            # Include gminiwob files for displaying web navigation environments
-            'domains/web_navigation/**/*.html',
-            'domains/web_navigation/**/*.css',
-            'domains/web_navigation/**/*.js',
-            'domains/web_navigation/**/*.png',
-            # Include all the motion files for the quadruped locomotion domain
-            'domains/quadruped_locomotion/motion_imitation/data/motions/*.txt',
-            # Include the netlist and initial placement files for the circuit training domain
-            'domains/circuit_training/**/*.pb.txt',
-            'domains/circuit_training/**/*.plc',
-            # Include package data from codecarbon's setup.py
-            'metrics/system/codecarbon/codecarbon/data/cloud/impact.csv',
-            'metrics/system/codecarbon/codecarbon/data/hardware/cpu_power.csv',
-            'metrics/system/codecarbon/codecarbon/data/private_infra/2016/usa_emissions.json',
-            'metrics/system/codecarbon/codecarbon/data/private_infra/2016/canada_energy_mix.json',
-            'metrics/system/codecarbon/codecarbon/data/private_infra/carbon_intensity_per_source.json',
-            'metrics/system/codecarbon/codecarbon/data/private_infra/global_energy_mix.json',
-            'metrics/system/codecarbon/codecarbon/viz/assets/*.png',
-        ]
+        "circuit_training": InstallCircuitTraining,
     },
 )
